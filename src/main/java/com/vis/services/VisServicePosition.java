16ad0c9b19027a2c8dea4cffc595c4c9f08db3d8
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2026-06-21 00:59:24 -0300

    RESEND TOKEN

--- src/main/java/com/vis/services/VisServicePosition.java
+++ src/main/java/com/vis/services/VisServicePosition.java
@@ -9,7 +9,7 @@ import com.ccp.decorators.CcpJsonRepresentation.CcpJsonFieldName;
 import com.ccp.dependency.injection.CcpDependencyInjection;
 import com.ccp.especifications.db.crud.CcpCrud;
 import com.ccp.especifications.db.crud.CcpGetEntityId;
-import com.ccp.especifications.db.crud.CcpGetEntityId.CcpSelectUnionAll;
+import com.ccp.especifications.db.crud.CcpSelectUnionAll;
 import com.ccp.especifications.db.utils.entity.CcpEntity;
 import com.jn.services.JnService;
 import com.jn.utils.JnDeleteKeysFromCache;
--- src/main/java/com/vis/utils/VisUtils.java
+++ src/main/java/com/vis/utils/VisUtils.java
@@ -18,7 +18,7 @@ import com.ccp.dependency.injection.CcpDependencyInjection;
 import com.ccp.especifications.db.bulk.CcpBulkEntityOperationType;
 import com.ccp.especifications.db.bulk.CcpBulkItem;
 import com.ccp.especifications.db.crud.CcpCrud;
-import com.ccp.especifications.db.crud.CcpGetEntityId.CcpSelectUnionAll;
+import com.ccp.especifications.db.crud.CcpSelectUnionAll;
 import com.ccp.especifications.db.crud.CcpUnionAllExecutor;
 import com.ccp.especifications.db.query.CcpQueryExecutor;
 import com.ccp.especifications.db.query.CcpQueryOptions;
