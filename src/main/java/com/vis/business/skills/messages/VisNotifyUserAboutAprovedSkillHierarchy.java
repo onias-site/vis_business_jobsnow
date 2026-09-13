72f75798a2e136882de64c12998c192cd54dc755
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2026-09-12 23:57:51 -0300

    reformando sistema de mensagens

--- src/main/java/com/vis/business/skills/messages/VisNotifyUserAboutAprovedSkillHierarchy.java
+++ src/main/java/com/vis/business/skills/messages/VisNotifyUserAboutAprovedSkillHierarchy.java
@@ -3,12 +3,11 @@ package com.vis.business.skills.messages;
 import com.jn.business.messages.JnBusinessSendMessage;
 import com.jn.business.messages.JnMessageSenderExceptionHandler;
 import com.jn.business.messages.JnMessageType;
-import com.vis.entities.VisEntitySkillFixHierarchyApproved;
 
 //FIXME FALTANDO TEMPLATE
 public class VisNotifyUserAboutAprovedSkillHierarchy extends JnBusinessSendMessage{
 	protected VisNotifyUserAboutAprovedSkillHierarchy() {
-		super(VisEntitySkillFixHierarchyApproved.ENTITY, JnMessageSenderExceptionHandler.THROWS);
+		super(JnMessageSenderExceptionHandler.THROWS);
 	}
 
 	public JnMessageType[] getMessageTypes() {
--- src/main/java/com/vis/business/skills/messages/VisNotifyUserAboutRejectedSkill.java
+++ src/main/java/com/vis/business/skills/messages/VisNotifyUserAboutRejectedSkill.java
@@ -3,12 +3,11 @@ package com.vis.business.skills.messages;
 import com.jn.business.messages.JnBusinessSendMessage;
 import com.jn.business.messages.JnMessageSenderExceptionHandler;
 import com.jn.business.messages.JnMessageType;
-import com.jn.entities.JnEntityUserRequest;
 
 //FIXME FALTANDO TEMPLATE
 public class VisNotifyUserAboutRejectedSkill extends JnBusinessSendMessage{
 	protected VisNotifyUserAboutRejectedSkill() {
-		super(JnEntityUserRequest.ENTITY, JnMessageSenderExceptionHandler.THROWS);
+		super(JnMessageSenderExceptionHandler.THROWS);
 	}
 	
 	public JnMessageType[] getMessageTypes() {
